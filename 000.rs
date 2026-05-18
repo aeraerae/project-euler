86ea2d4562c59e6b2b74f8eeb4d9fefa9a833d7a
aeraerae <gummi2999@gmail.com>
2026-05-18 20:36:53 +0900

    inital commit

---
+++ 000.rs
@@ -0,0 +1,42 @@
+#![allow(unused_imports, dead_code)]
+use std::{cmp::{Reverse, max, min}, collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDeque}, f64::consts::PI, fmt::{Write, format}, io::{Read, stdin}, iter::Peekable, mem::swap, str::{FromStr, SplitWhitespace}};
+
+fn main() {
+    let mut buf = String::new();
+    let mut out = String::new();
+
+    stdin().read_to_string(&mut buf).unwrap();
+    let mut scan = Scanner::new(buf.split_whitespace().peekable());
+
+    let mut tc = 0;
+    if tc != 1 { tc = scan.next::<usize>(); }
+    for i in 0..tc {
+        hyperbolic(&mut scan, &mut out, i);
+    }
+    print!("{out}");
+}
+
+// https://youtu.be/r89YusWbFZE
+fn hyperbolic<'a>(scan: &mut Scanner<'a, Peekable<SplitWhitespace<'a>>>, out: &mut String, _tc: usize) {
+    let n: usize = scan.next();
+
+    let mut ans = 0;
+    for i in 1..=n {
+        if i & 1 == 1 { ans += i * i; }
+    }
+
+    writeln!(out, "{ans}").unwrap();
+}
+
+struct Scanner<'a, I: Iterator<Item = &'a str>> {
+    iter: I,
+}
+
+impl<'a, I: Iterator<Item = &'a str>> Scanner<'a, I> {
+    fn new(from: I) -> Self {
+        Self { iter: from }
+    }
+    fn next<T: std::str::FromStr>(&mut self) -> T {
+        self.iter.next().unwrap().parse().ok().unwrap()
+    }
+}
