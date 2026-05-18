77a6b6c5288f28269a179c8861fe4d3c4d0dcef8
aeraerae <gummi2999@gmail.com>
2026-05-18 21:57:48 +0900

    005.rs 006.rs

---
+++ src/bin/005.rs
@@ -0,0 +1,57 @@
+#![allow(unused_imports, dead_code)]
+use std::{cmp::{Reverse, max, min}, collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDeque}, f64::consts::PI, fmt::{Write, format}, io::{Read, stdin}, iter::Peekable, mem::swap, str::{FromStr, SplitWhitespace}};
+
+fn main() {
+    let mut buf = String::new();
+    let mut out = String::new();
+
+    stdin().read_to_string(&mut buf).unwrap();
+    let mut scan = Scanner::new(buf.split_whitespace().peekable());
+
+    let mut tc = 1;
+    if tc != 1 { tc = scan.next::<usize>(); }
+    for i in 0..tc {
+        solve(&mut scan, &mut out, i);
+    }
+    print!("{out}");
+}
+
+// https://youtu.be/r89YusWbFZE
+fn solve<'a>(scan: &mut Scanner<'a, Peekable<SplitWhitespace<'a>>>, out: &mut String, _tc: usize) {
+    let n: usize = scan.next();
+    
+    let mut ans = 1;
+    for i in 2..=n {
+        ans = lcm(ans, i);
+    }
+    writeln!(out, "{ans}").unwrap();
+}
+
+fn gcd(mut a: usize, mut b: usize) -> usize {
+    if b > a {
+        (a, b) = (b, a);
+    }
+    if b == 0 { return a; }
+    while a % b != 0 {
+        let r = a % b;
+        a = b;
+        b = r; }
+    b 
+}
+
+fn lcm(a: usize, b: usize) -> usize {
+    a / gcd(a, b) * b
+}
+
+struct Scanner<'a, I: Iterator<Item = &'a str>> {
+    iter: I,
+}
+
+impl<'a, I: Iterator<Item = &'a str>> Scanner<'a, I> {
+    fn new(from: I) -> Self {
+        Self { iter: from }
+    }
+    fn next<T: std::str::FromStr>(&mut self) -> T {
+        self.iter.next().unwrap().parse().ok().unwrap()
+    }
+}
