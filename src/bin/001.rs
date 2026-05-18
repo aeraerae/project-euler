0766bd528c3f18f97ea22c718f20e63ced0f1971
aeraerae <gummi2999@gmail.com>
2026-05-18 21:19:36 +0900

    001.rs 002.rs

---
+++ src/bin/001.rs
@@ -0,0 +1,41 @@
+#![allow(unused_imports, dead_code)]
+use std::{cmp::{Reverse, max, min}, collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDeque}, f64::consts::PI, fmt::{Write, format}, io::{Read, stdin}, iter::Peekable, mem::swap, str::{FromStr, SplitWhitespace}};
+
+fn main() {
+    let mut buf = String::new();
+    let mut out = String::new();
+
+    stdin().read_to_string(&mut buf).unwrap();
+    let mut scan = Scanner::new(buf.split_whitespace().peekable());
+
+    let mut tc = 1;
+    if tc != 1 { tc = scan.next::<usize>(); }
+    for i in 0..tc {
+        solve(&mut scan, &mut out, i);
+    }
+    print!("{out}");
+}
+
+// https://youtu.be/r89YusWbFZE
+fn solve<'a>(scan: &mut Scanner<'a, Peekable<SplitWhitespace<'a>>>, out: &mut String, _tc: usize) {
+    let n: usize = scan.next();
+
+    let mut ans = 0;
+    for i in 1..n {
+        if i % 3 == 0 || i % 5 == 0 { ans += i; }
+    }
+    writeln!(out, "{ans}").unwrap();
+}
+
+struct Scanner<'a, I: Iterator<Item = &'a str>> {
+    iter: I,
+}
+
+impl<'a, I: Iterator<Item = &'a str>> Scanner<'a, I> {
+    fn new(from: I) -> Self {
+        Self { iter: from }
+    }
+    fn next<T: std::str::FromStr>(&mut self) -> T {
+        self.iter.next().unwrap().parse().ok().unwrap()
+    }
+}
---
+++ src/bin/002.rs
@@ -0,0 +1,45 @@
+#![allow(unused_imports, dead_code)]
+use std::{cmp::{Reverse, max, min}, collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDeque}, f64::consts::PI, fmt::{Write, format}, io::{Read, stdin}, iter::Peekable, mem::swap, str::{FromStr, SplitWhitespace}};
+
+fn main() {
+    let mut buf = String::new();
+    let mut out = String::new();
+
+    stdin().read_to_string(&mut buf).unwrap();
+    let mut scan = Scanner::new(buf.split_whitespace().peekable());
+
+    let mut tc = 1;
+    if tc != 1 { tc = scan.next::<usize>(); }
+    for i in 0..tc {
+        solve(&mut scan, &mut out, i);
+    }
+    print!("{out}");
+}
+
+// https://youtu.be/r89YusWbFZE
+fn solve<'a>(scan: &mut Scanner<'a, Peekable<SplitWhitespace<'a>>>, out: &mut String, _tc: usize) {
+    let n: usize = scan.next();
+
+    let mut ans = 0;
+    let mut prv = 1;
+    let mut cur = 2;
+    while cur <= n {
+        if cur & 1 == 0 { ans += cur; }
+        (prv, cur) = (cur, prv + cur);
+    }
+    
+    writeln!(out, "{ans}").unwrap();
+}
+
+struct Scanner<'a, I: Iterator<Item = &'a str>> {
+    iter: I,
+}
+
+impl<'a, I: Iterator<Item = &'a str>> Scanner<'a, I> {
+    fn new(from: I) -> Self {
+        Self { iter: from }
+    }
+    fn next<T: std::str::FromStr>(&mut self) -> T {
+        self.iter.next().unwrap().parse().ok().unwrap()
+    }
+}
